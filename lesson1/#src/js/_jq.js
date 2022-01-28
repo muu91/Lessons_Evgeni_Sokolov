d2e4a567b0cf4ae65abb35ae898a4c8a0a062314
Ulanbek <m.u.u.91@mail.ru>
2022-01-29 01:07:56 +0600

    Первая заливка Gulp (Lesson1)

---
+++ lesson1/#src/js/_jq.js
@@ -0,0 +1,280 @@
+
+$(document).ready(function () {
+
+    // ========================================
+    // ---------- VARIABLES ----------
+    let windowWidthJq = $(window).width();
+    // ---------- END VARIABLES ----------
+    // ========================================
+    // ---------- INDEX ----------
+    // ----------------------------------------
+    // ----- LESSON 1-12 -----
+    $('.btn_right12').click(function () {
+        $("[data-anim=blue_blocks__item]").each(function () {
+            let blue_blocks__item = $('[data-anim=blue_blocks__item]').length;
+            if (blue_blocks__item <= 2) {
+                $(this).next().attr('data-anim', 'blue_blocks__item');
+            }
+            if (blue_blocks__item > 1) {
+                $('.btn_right12').hide();
+            }
+        });
+    });
+    $('#lesson1_12 .swiper-button-next').click(function () {
+        $('.btn_right12').show();
+        let blue_blocks__item = $('[data-anim=blue_blocks__item]').length;
+        if (blue_blocks__item > 1) {
+            $('[data-anim=blue_blocks__item]:not(:first)').attr('data-anim', '');
+        }
+    });
+    // ----- END LESSON 1-12 -----
+    // ----------------------------------------
+    // ----- LESSON 1-18 -----
+    $('.btn_right18').click(function () {
+        $("[data-anim18=blue_blocks__item]").each(function () {
+            var blue_blocks__item = $('[data-anim18=blue_blocks__item]').length;
+            if (blue_blocks__item <= 2) {
+                $(this).next().attr('data-anim18', 'blue_blocks__item');
+            }
+            if (blue_blocks__item > 1) {
+                $('.btn_right18').hide();
+            }
+        });
+    });
+    $('#lesson1_18 .swiper-button-next').click(function () {
+        $('.btn_right18').show();
+        let blue_blocks__item = $('[data-anim18=blue_blocks__item]').length;
+        if (blue_blocks__item > 1) {
+            $('[data-anim18=blue_blocks__item]:not(:first)').attr('data-anim18', '');
+        }
+    });
+    // ----- END LESSON 1-18 -----
+    // ----------------------------------------
+    // ----- LESSON 1-23 -----
+    $('.btn_right23').click(function () {
+        $("[data-anim23=blue_blocks__item]").each(function () {
+            var blue_blocks__item = $('[data-anim23=blue_blocks__item]').length;
+            if (blue_blocks__item <= 2) {
+                $(this).next().attr('data-anim23', 'blue_blocks__item');
+            }
+            if (blue_blocks__item > 1) {
+                $('.btn_right23').hide();
+            }
+        });
+    });
+    $('#lesson1_23 .swiper-button-next').click(function () {
+        $('.btn_right23').show();
+        let blue_blocks__item = $('[data-anim23=blue_blocks__item]').length;
+        if (blue_blocks__item > 1) {
+            $('[data-anim23=blue_blocks__item]:not(:first)').attr('data-anim23', '');
+        }
+    });
+    // ----- END LESSON 1-23 -----
+    // ----------------------------------------
+    // ----- LESSON 1-32 -----
+    $('.btn_right32').click(function () {
+        $("[data-anim32=blue_blocks__item]").each(function () {
+            var blue_blocks__item = $('[data-anim32=blue_blocks__item]').length;
+            if (blue_blocks__item <= 1) {
+                $(this).next().attr('data-anim32', 'blue_blocks__item');
+            }
+            if (blue_blocks__item > 0) {
+                $('.btn_right32').hide();
+            }
+        });
+    });
+    $('#lesson1_32 .swiper-button-next').click(function () {
+        $('.btn_right32').show();
+        let blue_blocks__item = $('[data-anim32=blue_blocks__item]').length;
+        if (blue_blocks__item > 0) {
+            $('[data-anim32=blue_blocks__item]:not(:first)').attr('data-anim32', '');
+        }
+    });
+    // ----- END LESSON 1-32 -----
+    // ----------------------------------------
+    // ----- LESSON 1-26 -----
+    $('.conversion__center').click(function () {
+        $('.when_click').hide();
+        $(this).next().find('.conversion__right__body').removeClass('d-none');
+        $(this).find('img').css('transform', 'rotate(190deg)');
+    });
+
+    // ----- END LESSON 1-26 -----
+    // ----------------------------------------
+
+    // ---------- END INDEX ----------
+    // ========================================
+
+
+
+
+    // ========================================
+    // ---------- OTHER ----------
+    // ----------------------------------------
+    // ----- QUICK TEXTS -----
+    let txt_slide_1_6 = new Typed('#text_slide_1-6', {
+        strings: [
+            '',
+            'Вода – это основа всего живого на нашей планете. <br> <br> Вода нужна и растениям, и животным и людям.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_7 = new Typed('#text_slide_1-7', {
+        strings: [
+            '',
+            'Да, в целом, воды достаточно много – поверхность всех морей и океанов в два раза больше поверхности суши.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_8 = new Typed('#text_slide_1-8', {
+        strings: [
+            '',
+            'Но основная масса воды на Земле – соленая морская и океанская вода, а для поддержания жизни растений, животных и человека нужна вода пресная.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_9 = new Typed('#text_slide_1-9', {
+        strings: [
+            '',
+            'А такой воды всего 1% и каждый 3 человек на планете испытывает жажду.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_13 = new Typed('#text_slide_1-13', {
+        strings: [
+            '',
+            'Плотно закройте кран - капля за раз может слить до 200 литров за 24 часа.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_14 = new Typed('#text_slide_1-14', {
+        strings: [
+            '',
+            'Потому, что при выработке электричества расходуются уголь, газ, нефть и вода, запасы которых не безграничны, а выбросы в атмосферу вредных веществ огромны',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_15 = new Typed('#text_slide_1-15', {
+        strings: [
+            '',
+            'Жителям больших городов часто трудно дышать от смога. <br> <br> В результате сжигания топлива <br> и сокращения лесов на земле, <br> в атмосфере увеличивается <br> концентрация «парниковых газов», <br> а это ведет к потеплению и всеобщему изменению климата.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_20 = new Typed('#text_slide_1-20', {
+        strings: [
+            '',
+            'Потому, что на создание любой вещи, которая есть у нас дома, использовалось очень много электроэнергии и воды, а еще топлива, чтобы довезти её с завода в магазин.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_25 = new Typed('#text_slide_1-25', {
+        strings: [
+            '',
+            'Каждый день человечество производит несколько миллионов тонн мусора. Если его не утилизировать, через 20 лет вся планета превратится <br> в большую мусорную свалку. <br> <br> Но люди научились перерабатывать свой мусор - то есть делать из мусора полезные вещи. ',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+    let txt_slide_1_33 = new Typed('#text_slide_1-33', {
+        strings: [
+            '',
+            'Ты прошел вводный курс. <br> <br> Теперь можешь применить эти знания в своей жизни.',
+        ],
+        startDelay: 0,
+        typeSpeed: 40,
+        backSpeed: 0,
+        loop: false,
+        showCursor: false,
+    });
+    // ----------------------------------------
+
+
+
+    $('.swiper-button-next, .swiper-button-prev').click(function () {
+        if ($('#lesson1_6').hasClass('swiper-slide-active')) {
+            txt_slide_1_6.reset();
+        } else if ($('#lesson1_7').hasClass('swiper-slide-active')) {
+            txt_slide_1_7.reset();
+        } else if ($('#lesson1_8').hasClass('swiper-slide-active')) {
+            txt_slide_1_8.reset();
+        } else if ($('#lesson1_9').hasClass('swiper-slide-active')) {
+            txt_slide_1_9.reset();
+        } else if ($('#lesson1_13').hasClass('swiper-slide-active')) {
+            txt_slide_1_13.reset();
+        } else if ($('#lesson1_14').hasClass('swiper-slide-active')) {
+            txt_slide_1_14.reset();
+        } else if ($('#lesson1_15').hasClass('swiper-slide-active')) {
+            txt_slide_1_15.reset();
+        } else if ($('#lesson1_20').hasClass('swiper-slide-active')) {
+            txt_slide_1_20.reset();
+        } else if ($('#lesson1_25').hasClass('swiper-slide-active')) {
+            txt_slide_1_25.reset();
+        } else if ($('#lesson1_33').hasClass('swiper-slide-active')) {
+            txt_slide_1_33.reset();
+        }
+    });
+    // ----------------------------------------
+    // ----- END QUICK TEXTS -----
+    // ----------------------------------------
+
+    // $(function () {
+    //     setInterval(function () {
+    //         let animationName = 'animate__animated animate__tada';
+    //         let animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    //         $('.bottom__img img').addClass(animationName).one(animationend, function () {
+    //             $(this).removeClass(animationName);
+    //         });
+    //     }, 1000);
+    // })
+    // ---------- END OTHER ----------
+    // ========================================
+
+
+
+
+});
---
+++ lesson1/#src/js/_swiper.js
@@ -0,0 +1,253 @@
+
+// ========================================
+
+// ---------- VARIABLES ----------
+let windowWidthSwiper = document.documentElement.clientWidth;
+// ---------- END VARIABLES ----------
+// ========================================
+// ---------- INDEX ----------
+// ----------------------------------------
+// ----- HEADER -----
+let bl_lesson_1 = new Swiper('.bl_lesson_1', {
+  loop: false,
+  slidesPerView: 1,
+  // spaceBetween: 3000,
+  // effect: "fade",
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  speed: 800,
+  slidesPerGroup: 1,
+  allowTouchMove: false,
+  on: {
+    slideChangeTransitionStart: function () {
+      //     // $('.left_texts .zoom-in-right__p').delay(200).hide(0);
+      //     // $('.left_texts .zoom-in-right__p').removeClass('aos-init').removeClass('aos-animate');
+      //     // ----------------------------------------
+      //     // ----- SLIDE 1-10 -----
+      //     // $('.blue_blocks__item_active ').delay(200).hide(0);
+      //     // $('.blue_blocks__item_active ').removeClass('aos-init').removeClass('aos-animate');
+      //     // ----- END SLIDE 1-10 -----
+      //     // ----------------------------------------
+      //     // ----- SLIDE 31-32 -----
+      $('.two_block__item__active ').delay(200).hide(0);
+      $('.two_block__item__active ').removeClass('aos-init').removeClass('aos-animate');
+      $('.cups__item ').delay(200).hide(0);
+      $('.cups__item ').removeClass('aos-init').removeClass('aos-animate');
+
+      //     // ----- END SLIDE 31-32 -----
+      //     // ----------------------------------------
+    },
+    slideChangeTransitionEnd: function () {
+      //   // $('.left_texts p').delay(0).show(0);
+      //   // AOS.init();
+      //   // ----------------------------------------
+      //   // ----- SLIDE 1-10 -----
+      //   // $('.blue_blocks__item_active').show(0);
+      //   // ----- END SLIDE 1-10 -----
+      //   // ----------------------------------------
+      //   // ----- SLIDE 31-32 -----
+      $('.two_block__item__active ').show(0);
+      $('.cups__item ').show(0);
+      //   // ----- END SLIDE 31-32 -----
+      //   // ----------------------------------------
+      AOS.init();
+    },
+  }
+});
+
+// ----------------------------------------
+// ---------- SLIDE 26 ----------
+
+let conversion__center = document.querySelectorAll('.conversion__center');
+let conversion__center_l = conversion__center.length;
+
+conversion__center[0].addEventListener('click', () => {
+  const d_none = document.querySelectorAll('.conversions__items .d-none');
+  const d_none_l = d_none.length;
+  if (d_none_l <= 1) {
+    setTimeout(function () {
+      bl_lesson_1.slideNext();
+    }, 2000);
+  }
+});
+conversion__center[1].addEventListener('click', () => {
+  const d_none = document.querySelectorAll('.conversions__items .d-none');
+  const d_none_l = d_none.length;
+  if (d_none_l <= 1) {
+    setTimeout(function () {
+      bl_lesson_1.slideNext();
+    }, 2000);
+  }
+});
+conversion__center[2].addEventListener('click', () => {
+  const d_none = document.querySelectorAll('.conversions__items .d-none');
+  const d_none_l = d_none.length;
+  if (d_none_l <= 1) {
+    setTimeout(function () {
+      bl_lesson_1.slideNext();
+    }, 2000);
+  }
+});
+conversion__center[3].addEventListener('click', () => {
+  const d_none = document.querySelectorAll('.conversions__items .d-none');
+  const d_none_l = d_none.length;
+  if (d_none_l <= 1) {
+    setTimeout(function () {
+      bl_lesson_1.slideNext();
+    }, 2000);
+  }
+});
+// ---------- END SLIDE 26 ----------
+// ----------------------------------------
+// ---------- SLIDE 26-31 ----------
+
+// const swiper_slide = document.querySelectorAll('.bl_lesson_1 .swiper-wrapper .swiper-slide');
+
+// const lesson1_27 = document.getElementById('lesson1_27');
+// const lesson1_28 = document.getElementById('lesson1_28');
+// const lesson1_29 = document.getElementById('lesson1_29');
+// const lesson1_30 = document.getElementById('lesson1_30');
+
+// const lesson1_27_index = Array.from(swiper_slide).indexOf(lesson1_27);
+// const lesson1_28_index = Array.from(swiper_slide).indexOf(lesson1_28);
+// const lesson1_29_index = Array.from(swiper_slide).indexOf(lesson1_29);
+// const lesson1_30_index = Array.from(swiper_slide).indexOf(lesson1_30);
+
+// const btn_to_27 = document.querySelectorAll('.slide_to_27');
+// const btn_to_28 = document.querySelectorAll('.slide_to_28');
+// const btn_to_29 = document.querySelectorAll('.slide_to_29');
+// const btn_to_30 = document.querySelectorAll('.slide_to_30');
+
+// console.log(btn_to_30.length)
+
+// btn_to_27[0].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_27_index);
+// });
+// btn_to_27[1].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_27_index);
+// });
+// btn_to_27[2].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_27_index);
+// });
+// btn_to_27[3].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_27_index);
+// });
+// btn_to_27[4].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_27_index);
+// });
+
+// btn_to_28[0].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_28_index);
+// });
+// btn_to_28[1].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_28_index);
+// });
+// btn_to_28[2].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_28_index);
+// });
+// btn_to_28[3].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_28_index);
+// });
+// btn_to_28[4].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_28_index);
+// });
+
+
+// btn_to_29[0].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_29_index);
+// });
+// btn_to_29[1].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_29_index);
+// });
+// btn_to_29[2].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_29_index);
+// });
+// btn_to_29[3].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_29_index);
+// });
+// btn_to_29[4].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_29_index);
+// });
+
+// btn_to_30[0].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_30_index);
+// });
+// btn_to_30[1].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_30_index);
+// });
+// btn_to_30[2].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_30_index);
+// });
+// btn_to_30[3].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_30_index);
+// });
+// btn_to_30[4].addEventListener('click', () => {
+//   bl_lesson_1.slideTo(lesson1_30_index);
+// });
+
+// Автоскролл в следующий слайд
+// const lesson1_31 = document.getElementById('lesson1_31');
+// const lesson1_31_index = Array.from(swiper_slide).indexOf(lesson1_31);
+
+// bl_lesson_1.on("slideChange", function () {
+//   if (this.activeIndex == lesson1_31_index - 1) {
+//     setTimeout(function () {
+//       bl_lesson_1.slideNext();
+//     }, 5000);
+//   }
+// });
+
+
+// ---------- END SLIDE 26-31 ----------
+// ----------------------------------------
+// AOS.init();
+
+// ----- END HEADER -----
+// ----- BRANCHES -----
+// const swiper_bl_branches = new Swiper('.bl_branches__swiper', {
+//   loop: false,
+//   slidesPerView: 3,
+//   // spaceBetween: 3000,
+//   // effect: "fade",
+//   // pagination: {
+//   //   el: '.swiper-pagination',
+//   // },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+//   speed: 800,
+//   spaceBetween: 30,
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     draggable: true
+//   },
+//   breakpoints: {
+//     320: {
+//       // slidesPerView: 'auto',
+//       spaceBetween: 10,
+//     },
+//     // 500: {
+//     //   // slidesPerView: 3,
+//     //   spaceBetween: 10,
+
+//     // },
+//     1024: {
+//       spaceBetween: 20,
+
+//     },
+//     1200: {
+//       spaceBetween: 30,
+//     },
+//   },
+// });
+// ----- END BRANCHES -----
+// ----------------------------------------
+// ---------- END INDEX ----------
+// ========================================
